e6d800086a3fbd4778382da0e928f0b20a2fa745
ritevi <sanekv1999@gmail.com>
2020-12-13 05:32:48 +0300

    errors

--- libs/Connector/index.js
+++ libs/Connector/index.js
@@ -38,7 +38,7 @@ class ConnectorWS extends wsServer{
                         break;
                 }
             } catch (err){
-                socket.send({type:"error",body:err});
+                socket.send({type:"error",body:err.toString()});
                 this.emit("error",err);
             }
         });
@@ -57,7 +57,7 @@ class ConnectorWS extends wsServer{
                         break;
                 }
             } catch (err){
-                socket.send({type:"authRobotError",body:err})
+                socket.send({type:"authRobotError",body:err.toString()})
             }
         })
     }
@@ -77,7 +77,7 @@ class ConnectorWS extends wsServer{
                         break;
                 }
             } catch (err){
-                socket.send({type:"authUserError",body:err})
+                socket.send({type:"authUserError",body:err.toString()})
             }
 
         })
@@ -88,7 +88,7 @@ class ConnectorWS extends wsServer{
             if (cmd) {
                 let robotUuid = this.userToRobot.get(userId);
                 let robotSocket = this.robotMap.get(robotUuid);
-                if (!robotSocket) robotSocket.send({type: "error", body: new Error("no robot socket")});
+                if (!robotSocket) robotSocket.send({type: "error", body: new Error("no robot socket").toString()});
                 robotSocket.send(JSON.stringify({body: cmd, type: "cmd"}));
             } else {
                 throw new Error("no cmd");
@@ -103,14 +103,14 @@ class ConnectorWS extends wsServer{
             if (info) {
                 let userId = this.robotToUser.get(uuid);
                 let userSocket = this.userMap.get(userId);
-                if (!userSocket) userSocket.send({type: "infoError", body: new Error("no user socket")});
+                if (!userSocket) userSocket.send({type: "infoError", body: new Error("no user socket").toString()});
                 userSocket.send(JSON.stringify({body: info, type: "info"}));
             } else {
                 throw new Error("no info");
             }
         } catch (err){
             let robotSocket = this.robotMap.get(uuid);
-            robotSocket.send({type:"infoError",body:err});
+            robotSocket.send({type:"infoError",body:err.toString()});
         }
     }
 
@@ -128,7 +128,7 @@ class ConnectorWS extends wsServer{
                 this.emit("robotAdd", uuid, socket);
             }
         }catch (err){
-            socket.send({type:"authRobotError",body:err});
+            socket.send({type:"authRobotError",body:err.toString()});
         }
     }
 
@@ -153,7 +153,7 @@ class ConnectorWS extends wsServer{
             })
         } catch (err){
             this.emit("error",err);
-            socket.send({type:"authUserError",body:err});
+            socket.send({type:"authUserError",body:err.toString()});
             socket.destroy();
         }
 
