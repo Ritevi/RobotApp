50152eb6ec38653684e98def7a4fecfe30190c73
ritevi <sanekv1999@gmail.com>
2021-03-31 07:28:31 +0300

    ADD mail class for sending emails

--- libs/Email/index.js
+++ libs/Email/index.js
@@ -1,14 +1,40 @@
 const nodemailer = require('nodemailer');
+const config = require('../../config');
 
 class mail {
   constructor() {
     this.transporter = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
-      port: 587,
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      service: 'gmail',
       auth: {
-        user: 'cleora98@ethereal.email',
-        pass: '991YRgXy9KEm44jePW',
+        type: 'OAuth2',
+        user: 'sanekv1999@gmail.com',
+        clientId: config.get('ClientIdGmail'),
+        clientSecret: config.get('ClientSecretGmail'),
+        refreshToken: config.get('RefreshTokenGmail'),
+        accessToken: config.get('AccessTokenGmail'),
+        expires: 1484314697598,
       },
+    });
+  }
+
+  async sendEmail(email, text, html = '<h1>Hi ksta</h1>', subject = 'RobotApp') {
+    return new Promise((resolve, reject) => {
+      this.transporter.sendMail({
+        from: 'sanekv1999@gmail.com', // sender address
+        to: email, // list of receivers
+        subject, // Subject line
+        text, // plain text body
+        html, // html body
+      }, (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res);
+        }
+      });
     });
   }
 }
