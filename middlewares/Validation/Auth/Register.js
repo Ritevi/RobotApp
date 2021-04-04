59e5bc73df8e0da4a08862fa4b1778a3f6dace15
ritevi <sanekv1999@gmail.com>
2021-04-04 22:50:43 +0300

    Add email required to validation

--- middlewares/Validation/Auth/Register.js
+++ middlewares/Validation/Auth/Register.js
@@ -3,6 +3,7 @@ const joi = require('joi');
 const RegisterSchema = joi.object({
   username: joi.string().required(),
   password: joi.string().required(),
+  email: joi.string().email().required(),
 });
 
 module.exports = async (req, res, next) => {
