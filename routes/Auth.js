f8bffd9d2da39521c35a65eb7eae643bac08912b
ritevi <sanekv1999@gmail.com>
2021-04-04 22:51:53 +0300

    Add activate profile route

--- routes/Auth.js
+++ routes/Auth.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const loginController = require('../controllers/Auth/Login');
 const refreshController = require('../controllers/Auth/RefreshTokens');
 const registerController = require('../controllers/Auth/Register');
+const activateController = require('../controllers/Auth/activateProfile');
 const authMiddleware = require('../middlewares/Auth');
 const { loginMiddleware, registerMiddleware, refreshTokenMiddleware } = require('../middlewares/Validation/Auth');
 
@@ -16,6 +17,8 @@ router.post('/login', loginMiddleware, loginController(AuthService));
 router.post('/register', registerMiddleware, registerController(AuthService));
 
 router.post('/refresh', refreshTokenMiddleware, refreshController(AuthService));
+
+router.get('/activateProfile/:emailUUID', activateController(AuthService));
 
 router.get('/private', authMiddleware(AccessStorage), (req, res) => {
   res.json({ message: 'hi ksta' });
